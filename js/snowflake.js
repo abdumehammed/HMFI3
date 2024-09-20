dd1b9cdabea3b93a61b8acf39c20d1f563b92848
abdumehammed <abdumehammed@gmail.com>
2024-09-20 15:35:38 +0300

    hello ;

--- js/snowflake.js
+++ js/snowflake.js
@@ -3,11 +3,14 @@ const snowflakeImageSrc = 'img/Adey.png'; // Path to your snowflake image
 const numberOfSnowflakes = 20; // Number of snowflakes to repeat
 
 for (let i = 0; i < numberOfSnowflakes; i++) {
-  // Create a new image elementgit
+  // Create a new image element
   const snowflake = document.createElement('img');
   snowflake.src = snowflakeImageSrc;
   snowflake.classList.add('snowflake');
-  
+
+  // Set the alt attribute
+  snowflake.setAttribute('alt', 'Adey Abeba');
+
   // Randomize the starting position, speed, and delay
   snowflake.style.left = Math.random() * window.innerWidth + 'px';
   snowflake.style.animationDuration = Math.random() * 3 + 2 + 's'; // Random speed between 2-5 seconds
