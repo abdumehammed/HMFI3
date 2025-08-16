cb39d0d2b6b3f3c67cd40f90d9f896075121a915
abdumehammed <abdumehammed@gmial.com>
2025-08-16 09:29:57 +0300

    removed the tawk.to chat

--- contact.php
+++ contact.php
@@ -441,7 +441,7 @@
 
 
     <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
+    <!-- <script type="text/javascript">
         var Tawk_API = Tawk_API || {},
             Tawk_LoadStart = new Date();
         (function() {
@@ -453,7 +453,7 @@
             s1.setAttribute('crossorigin', '*');
             s0.parentNode.insertBefore(s1, s0);
         })();
-    </script>
+    </script> -->
     <!--End of Tawk.to Script-->
 </body>
 
